4eb5b07030c950d5c52ab927a968e432c83cab31
goncalochincho <goncalochincho@gmail.com>
2018-05-08 14:55:07 +0100

    bug in instrument.sh

--- worker/instrument.sh
+++ worker/instrument.sh
@@ -1,2 +1,3 @@
-java -cp ../instrumentation/:src/pt/ulisboa/tecnico/meic/cnv/mazerunner/maze/strategies/ OpcodeMix src/pt/ulisboa/tecnico/meic/cnv/mazerunner/maze/strategies/ output
-cp output/* src/pt/ulisboa/tecnico/meic/cnv/mazerunner/maze/strategies/
+#!/bin/bash
+java -cp ../instrumentation/:src/main/java/pt/ulisboa/tecnico/meic/cnv/mazerunner/maze/strategies/ OpcodeMix src/main/java/pt/ulisboa/tecnico/meic/cnv/mazerunner/maze/strategies/ output
+cp output/* src/main/java/pt/ulisboa/tecnico/meic/cnv/mazerunner/maze/strategies/
