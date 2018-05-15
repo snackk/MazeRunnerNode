dab60fc2bc908f3a66a5ddb7125845b54f0cb9fb
goncalochincho <goncalochincho@gmail.com>
2018-05-15 22:36:11 +0100

    restructure

--- nodeMazeRunner/compile.sh
+++
@@ -1,1 +0,0 @@
-find -name "*.java" | xargs javac -cp /home/ec2-user/worker/src/main/java/:/home/ec2-user/aws-java-sdk-1.11.328/lib/aws-java-sdk-1.11.328.jar:/home/ec2-user/aws-java-sdk-1.11.328/third-party/lib/*
